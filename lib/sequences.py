392c45c5ace133e160c1e32c4cfae12a4f1d66ef
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-05-20 13:20:56 +1000

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]
+    while len(fibonacci) < length:
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    print(fibonacci[:length])
